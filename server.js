17ebadf7f8a7e3fd0f3c867f24cbd787e8b9b08d
jacoblf314 <jacoblf314@gmail.com>
2024-09-19 10:51:22 -0400

    server

--- server.js
+++ server.js
@@ -303,10 +303,53 @@ const server = createServer(async (req, res) => {
     accessControlConfig(req, res);
     if (!CORS_Preflight(req, res))
         if (!await handleRequest(req, res)) {
+            serveStaticFiles(req, res);
             res.writeHead(404);
             res.end();
         }
 });
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+function serveStaticFiles(req, res) {
+    const filePath = path.join(__dirname, 'Bookmarks-Manager-SPA', req.url === '/' ? 'index.html' : req.url);
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.ico':
+            contentType = 'image/x-icon';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('404 Not Found', 'utf-8');
+            } else {
+                res.writeHead(500);
+                res.end('500 Server Error: ' + err.code);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+}
 
